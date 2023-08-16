fe7648611b7ab7b97f234cfd62413fcad00205c7
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-16 12:09:37 -0500

    Proyecto Inicial

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'firestore-storage-angular',
+    appId: '1:688824261358:web:a5163513cb78bc031f71ff',
+    storageBucket: 'firestore-storage-angular.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCc8n4M3CismLvmcKWyc3oPXnnbqIJZfvs',
+    authDomain: 'firestore-storage-angular.firebaseapp.com',
+    messagingSenderId: '688824261358',
+  },};
