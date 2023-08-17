a52f63d860e5a43d5f64ebef936a4421da0a0c6c
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-17 11:40:18 -0500

    Iniciando Interfaces

---
+++ src/app/interfaces/place.interface.ts
@@ -0,0 +1,23 @@
+//Interfaz Base para Modelar nuestros datos - Base Modelo de Nuestros Objetos.
+//Podemos extender los datos si es necesario con otra interfaz usando extends o integrar una interfaz y funciones dentro de otra para hacerla más robusta.
+
+// Caso Practico:
+// INTERFACES DE TIPO: ATRACTIVO TURÍSTICO - PRESTADOR TURISTICO - MUNICIPIO - RUTAS TURÍSTICAS.
+// BASADOS EN SUS DEBIDOS FORMULARIOS A TENER EN CUENTA.
+
+//Iniciamos con una interfaz con propiedades compartidas
+export default interface Place {
+  id?: string;
+  name: string;
+  descripcion: string;
+  // latitude: number;
+  // longitude: number;
+  pathImage: string[]; //Array de Paths de Imágenes
+}
+
+//Interfaz para Atractivos Turísticos
+interface Attraction extends Place {}
+
+
+//Se puede crear otro tipo de interfaces con el fin de modelar otros tipos de datos distintos
+//Ejemplos sería categorías
