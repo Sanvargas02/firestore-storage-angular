c1691518734d65fe3c2558d1e26fb141f69f9ee0
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-30 11:57:24 -0500

    Organización de Carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoComponent } from './admin-dashboard/components/listado/listado.component';
+import { LoginComponent } from './auth/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //Path en caso de que no se coloque nada
+    path: '',
+    redirectTo: '/admin-dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin-dashboard',
+    component: ListadoComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+
 import { ListadoComponent } from './admin-dashboard/components/listado/listado.component';
 import { AgregarComponent } from './admin-dashboard/components/agregar/agregar.component';
 import { LoginComponent } from './auth/components/login/login.component';
 import { RecuperacionComponent } from './auth/components/recuperacion/recuperacion.component';
+
+import { AdminDashboardModule } from './admin-dashboard/admin-dashboard.module';
 
 @NgModule({
   declarations: [
@@ -27,7 +31,8 @@ import { RecuperacionComponent } from './auth/components/recuperacion/recuperaci
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
+    AdminDashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
