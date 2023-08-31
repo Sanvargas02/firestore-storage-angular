d631c79bbe6cd5e35f86f9c163a1296587238047
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-31 16:02:25 -0500

    Rutas Funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,18 @@ const routes: Routes = [
   //Quiero cargar el módulo de autenticación
   {
     path: 'auth', //Es un nombre cualquiera, pero lo llamo como mi módulo de autenticación
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ) //Importamos el Módulo Hijo
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ) //Importamos el Módulo Hijo.
   },
   {
-    path: '**',
+    path: 'productos', //Es un nombre cualquiera, pero lo llamo como mi módulo de productos
+    loadChildren: () => import('./productos/productos.module').then( m => m.ProductosModule ) //Importamos el Módulo Hijo.
+  },
+  {
+    path: 'usuarios', //Es un nombre cualquiera, pero lo llamo como mi módulo de usuarios
+    loadChildren: () => import('./usuarios/usuarios.module').then( m => m.UsuariosModule ) //Importamos el Módulo Hijo.
+  },
+  {
+    path: '**', //Path por defecto cuando ponen cualquier cosa o nada
     redirectTo: 'auth'
   }
 
---
+++ src/app/productos/productos.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductosRoutingModule } from './productos-routing.module';
+
+//Rutas
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { EditarComponent } from './pages/editar/editar.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    EditarComponent,
+    ListadoComponent,
+    ProductoComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductosRoutingModule
+  ]
+})
+export class ProductosModule { }
